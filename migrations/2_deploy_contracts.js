a870c34340681f7eaf9fd9a5262dbb3427a3d6fe
krouspy <knjlau1@gmail.com>
2020-02-28 16:57:13 +0100

    fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,15 @@
-var ERC721 = artifacts.require("./token/ERC721/ERC721Mintable.sol");
-var fragmentClaimer = artifacts.require(
+const web3 = require("web3");
+
+const ERC721 = artifacts.require(
+  "./token/ERC721/ERC721MetadataMintable/ERC721MetadataMintable.sol"
+);
+const fragmentClaimer = artifacts.require(
   "./fragment-claimer/fragmentClaimer.sol"
 );
 
 async function doDeploy(deployer, network, accounts) {
-  // Deployer ERC721
-  ERC721Contract = await deployer.deploy(ERC721);
-  // Deploy fragmentClaimer
-  fragmentClaimerContract = await deployer.deploy(
+  const ERC721Contract = await deployer.deploy(ERC721, "jsp", "gro");
+  const fragmentClaimerContract = await deployer.deploy(
     fragmentClaimer,
     500,
     ERC721Contract.address
@@ -15,10 +17,20 @@ async function doDeploy(deployer, network, accounts) {
   // Declare fragmentClaimer as minter for ERC721
   await ERC721Contract.addMinter(fragmentClaimerContract.address);
 
-  await fragmentClaimerContract.claimAToken(10,)
+  await fragmentClaimerContract.claimAToken(
+    10,
+    "uri",
+    web3.utils.fromAscii("1234")
+  );
 }
+
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
     await doDeploy(deployer, network, accounts);
   });
 };
+/*
+module.exports = deployer => {
+  deployer.deploy(ERC721, "jsp", "gro");
+};
+*/
