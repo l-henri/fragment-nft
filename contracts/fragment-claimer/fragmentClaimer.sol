f40ca52cf40dd777eac41464b6361e47951c56c3
krouspy <knjlau1@gmail.com>
2020-02-29 12:23:09 +0100

    try to add signature

--- contracts/fragment-claimer/fragmentClaimer.sol
+++ contracts/fragment-claimer/fragmentClaimer.sol
@@ -1,8 +1,7 @@
 pragma solidity ^0.5.0;
 
-// import "../token/ERC721/ERC721MetadataMintable/ERC721MetadataMintable.sol";
-import "../ERC721/ERC721Mintable/ERC721Mintable.sol";
-
+import "../ERC721/ERC721MetadataMintable/ERC721MetadataMintable.sol";
+import "./signatureTester.sol";
 
 /**
  * @title ERC721 Non-Fungible Token Standard basic implementation
@@ -17,14 +16,17 @@ contract fragmentClaimer {
     mapping(address => bool) public whitelist;
     mapping(uint => bool) public tokensThatWereClaimed;
 
+    SignatureTester private signatureTester;
+
     uint maxTokenId;
     uint totalFunds;
     address ERC721address;
 
-    constructor(uint _maxTokenId, address _ERC721address) public {
+    constructor(uint _maxTokenId, address _ERC721address, SignatureTester _signatureTester) public {
         whitelist[msg.sender] = true;
         maxTokenId = _maxTokenId;
         ERC721address = _ERC721address;
+        signatureTester = _signatureTester;
     }
 
     function () external payable {
@@ -34,83 +36,74 @@ contract fragmentClaimer {
         }
     }
 
-
-    function claimAToken(uint _tokenToClaim, bytes memory _signature) 
-    // function claimAToken(uint _tokenToClaim, string memory _tokenURI, bytes memory _signature) 
-    public 
+    // function claimToken(uint _tokenId, string memory _tokenURI, bytes memory _signature)
+    function claimToken(uint _tokenId, string memory _tokenURI)
+    public
     payable
     returns (bool)
     {
-
-        // Checking if the token has already been claimed
-        require(!tokensThatWereClaimed[_tokenToClaim], "Claim: token already claimed");
-        // Not sure this is useful but oh well
-        require(_tokenToClaim <= maxTokenId, "Claim: tokenId outbounds");
+        require(!tokensThatWereClaimed[_tokenId], "Claim: token already claimed");
+        require(_tokenId <= maxTokenId, "Claim: tokenId outbounds");
         // Creating a hash unique to this token number, and this token contract
-        // bytes32 _hash = keccak256(abi.encode(ERC721address, _tokenToClaim, _tokenURI));
-        bytes32 _hash = keccak256(abi.encode(ERC721address, _tokenToClaim));
+        bytes32 _hash = keccak256(abi.encode(ERC721address, _tokenId, _tokenURI));
         // Making sure that the signer has been whitelisted
+        signatureTester.getHash(ERC721address, _tokenId, _tokenURI);
+        bytes memory _signature = signatureTester.compiledHash;
         require(signerIsWhitelisted(_hash, _signature), "Claim: signer not whitelisted");
         // All should be good, so we mint a token yeah
-        // ERC721MetadataMintable targetERC721Contract = ERC721MetadataMintable(ERC721address);
-        ERC721Mintable targetERC721Contract = ERC721Mintable(ERC721address);
-        // targetERC721Contract.mintWithTokenURI(msg.sender, _tokenToClaim, _tokenURI);
-        targetERC721Contract.mint(msg.sender, _tokenToClaim);
-
+        ERC721MetadataMintable targetERC721Contract = ERC721MetadataMintable(ERC721address);
+        targetERC721Contract.mintWithTokenURI(msg.sender, _tokenId, _tokenURI);
         // Registering that the token was claimed
         // Note that there is a check in the ERC721 for this too
-        tokensThatWereClaimed[_tokenToClaim] = true;
+        tokensThatWereClaimed[_tokenId] = true;
         // Emitting an event
-        emit aTokenWasClaimed(_tokenToClaim, msg.sender);
-
+        emit aTokenWasClaimed(_tokenId, msg.sender);
         // If a tip was included, thank the tipper
         if (msg.value > 0) {
             emit yeeeeeeaaaaaahThxCoeurCoeurCoeur(msg.sender);
             totalFunds += msg.value;
         }
-        
     }
 
     function withdrawTips() public onlyWhitelisted {
         msg.sender.transfer(totalFunds);
-        emit withdrawFunds(msg.sender, totalFunds );
+        emit withdrawFunds(msg.sender, totalFunds);
     }
-
 
     // 20/02/27: Borrowed from https://github.com/austintgriffith/bouncer-proxy/blob/master/BouncerProxy/BouncerProxy.sol
     //borrowed from OpenZeppelin's ESDA stuff:
     //https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/cryptography/ECDSA.sol
     function signerIsWhitelisted(bytes32 _hash, bytes memory _signature) internal view returns (bool) {
-		bytes32 r;
-		bytes32 s;
-		uint8 v;
-		// Check the signature length
-		if (_signature.length != 65) {
-			return false;
-		}
-		// Divide the signature in r, s and v variables
-		// ecrecover takes the signature parameters, and the only way to get them
-		// currently is to use assembly.
-		// solium-disable-next-line security/no-inline-assembly
-		assembly {
-			r := mload(add(_signature, 32))
-			s := mload(add(_signature, 64))
-			v := byte(0, mload(add(_signature, 96)))
-		}
-		// Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-		if (v < 27) {
-			v += 27;
-		}
-		// If the version is correct return the signer address
-		if (v != 27 && v != 28) {
-			return false;
-		} else {
-			// solium-disable-next-line arg-overflow
-			return whitelist[ecrecover(keccak256(
-				abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)
-				), v, r, s)];
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // Check the signature length
+        if (_signature.length != 65) {
+            return false;
         }
-	}
+        // Divide the signature in r, s and v variables
+        // ecrecover takes the signature parameters, and the only way to get them
+        // currently is to use assembly.
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            r := mload(add(_signature, 32))
+            s := mload(add(_signature, 64))
+            v := byte(0, mload(add(_signature, 96)))
+        }
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        if (v < 27) {
+            v += 27;
+        }
+        // If the version is correct return the signer address
+        if (v != 27 && v != 28) {
+            return false;
+        } else {
+            // solium-disable-next-line arg-overflow
+            return whitelist[ecrecover(keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)
+            ), v, r, s)];
+        }
+    }
 
 	//  20/02/27: Borrowed from https://github.com/rocksideio/contracts/blob/master/contracts/Identity.sol
 	function updateWhitelist(address _account, bool _value) onlyWhitelisted public returns (bool) {
--- contracts/fragment-claimer/signatureTester.sol
+++ contracts/fragment-claimer/signatureTester.sol
@@ -1,20 +1,16 @@
+pragma solidity ^0.5.0;
 // Permet de tester a quoi ressemble un message empaquete et hashé
 
-contract signatureTester {
-  bytes32 public compiledHash;
-  bytes public encoded;
-    constructor() public
-    {
-     
-    }
+contract SignatureTester {
+    bytes32 public compiledHash;
+    bytes public encoded;
 
+    constructor() public {}
 
-
-    function getHash(address _contractAddress, uint _tokenNumber, string memory _tokenURI) 
-    public 
-    {
+    function getHash(address _contractAddress, uint _tokenNumber, string memory _tokenURI) public returns (bytes memory) {
         encoded = abi.encode(_contractAddress, _tokenNumber, _tokenURI);
         compiledHash = keccak256(encoded);
+        return compiledHash;
     }
 
 }
