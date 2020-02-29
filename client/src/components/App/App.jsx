6842b47d532e0597285a580a2ad6447d9b84232a
krouspy <knjlau1@gmail.com>
2020-03-01 00:50:51 +0100

    get web3 provider

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import getWeb3 from "#root/getWeb3";
+import FragmentClaimerContract from "#contractAbis/FragmentClaimer";
+
+import Layout from "../Layout";
+import Art from "../Art";
 
 export const App = () => {
-  return <h1>Hello World!</h1>;
+  const [data, setData] = useState({
+    web3: null,
+    fragmentClaimer: null,
+    accounts: []
+  });
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = FragmentClaimerContract.networks[networkId];
+        const fragmentClaimer = new web3.eth.Contract(
+          FragmentClaimerContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+        setData({ web3, fragmentClaimer, accounts });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    init();
+  }, []);
+
+  const { web3, fragmentClaimer, accounts } = data;
+
+  return (
+    <Layout>
+      <Art web3={web3} fragmentClaimer={fragmentClaimer} />
+    </Layout>
+  );
 };
