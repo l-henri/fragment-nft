750a90d53b1683dde963ebdd89bec4588b3412da
krouspy <knjlau1@gmail.com>
2020-02-29 12:22:17 +0100

    fix claimFragment call

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,3 @@
-const Web3 = require("web3");
-
-
 const ERC721 = artifacts.require(
   "./ERC721/ERC721MetadataMintable/ERC721MetadataMintable.sol"
 );
@@ -8,27 +5,50 @@ const fragmentClaimer = artifacts.require(
   "./fragment-claimer/fragmentClaimer.sol"
 );
 
-async function doDeploy(deployer, network, accounts) {
-  const ERC721Contract = await deployer.deploy(ERC721, "jsp", "gro");
-  const fragmentClaimerContract = await deployer.deploy(
-    fragmentClaimer,
-    500,
-    ERC721Contract.address
+const tokenNumber1 = 10;
+const tokenNumber2 = 20;
+const uri1 = "http://www.mescouilles.com";
+const uri2 = "http://www.mesboules.com";
+
+async function sign(web3, contractAddress, number, uri, account) {
+  const encoded = web3.eth.abi.encodeParameters(
+    ["address", "uint256", "string"],
+    [contractAddress, number, uri]
   );
-  // Declare fragmentClaimer as minter for ERC721
-  await ERC721Contract.addMinter(fragmentClaimerContract.address);
+  const hashToSign = web3.utils.keccak256(encoded);
+  return web3.eth.sign(hashToSign, account);
+}
 
-  // Testing to claim a token
-	const tokenNumber = 10
-	// const tokenURI = "http://www.mescouilles.com"
-	// const parametersEncoded = web3.eth.abi.encodeParameters(['address', 'uint256', 'string'], [ERC721.address, tokenNumber, tokenURI]);
-	const parametersEncoded = web3.eth.abi.encodeParameters(['address', 'uint256'], [ERC721.address, tokenNumber]);
-	const hashToSign = web3.utils.keccak256(parametersEncoded)
-	const signature = await web3.eth.sign(hashToSign,accounts[0])
-	console.log(signature)
-  	// await fragmentClaimerContract.claimAToken(tokenNumber, tokenURI, signature)
-  	await fragmentClaimerContract.claimAToken(tokenNumber, signature)
-
+async function doDeploy(deployer, network, accounts) {
+  if (network === "ganache") {
+    const ERC721Contract = await deployer.deploy(ERC721, "jsp", "gro");
+    const fragmentClaimerContract = await deployer.deploy(
+      fragmentClaimer,
+      500,
+      ERC721Contract.address
+    );
+    // Declare fragmentClaimer as minter for ERC721
+    await ERC721Contract.addMinter(fragmentClaimerContract.address);
+    // claim tokens
+    const signature1 = await sign(
+      web3,
+      ERC721.address,
+      tokenNumber1,
+      uri1,
+      accounts[0]
+    );
+    const signature2 = await sign(
+      web3,
+      ERC721.address,
+      tokenNumber2,
+      uri2,
+      accounts[0]
+    );
+    await fragmentClaimerContract.claimAToken(tokenNumber1, uri1, signature1);
+    await fragmentClaimerContract.claimAToken(tokenNumber2, uri2, signature2);
+  } else {
+    console.log("not ganache network");
+  }
 }
 
 module.exports = (deployer, network, accounts) => {
@@ -36,8 +56,3 @@ module.exports = (deployer, network, accounts) => {
     await doDeploy(deployer, network, accounts);
   });
 };
-/*
-module.exports = deployer => {
-  deployer.deploy(ERC721, "jsp", "gro");
-};
-*/
