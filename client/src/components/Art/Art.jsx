6385f29fa84ac72206be1cba03fbfd655160bfc0
krouspy <knjlau1@gmail.com>
2020-03-01 20:15:35 +0100

    add Paper

--- client/src/components/Art/Art.jsx
+++ client/src/components/Art/Art.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 import Row from "./Row";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    width: "75vw",
-    height: "85vh",
-    backgroundColor: "red"
+  content: {
+    width: "65vw",
+    height: "93vh",
+    margin: "0px 15px"
   }
 }));
 
@@ -16,22 +17,24 @@ export const Art = ({ fragmentClaimer }) => {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="center"
-      className={classes.root}
-    >
-      {Array(4)
-        .fill(0)
-        .map((_, index) => (
-          <Row
-            key={index}
-            fragmentClaimer={fragmentClaimer}
-            rowNumber={index}
-          />
-        ))}
-    </Grid>
+    <Paper elevation={3}>
+      <Grid
+        className={classes.content}
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        {Array(18)
+          .fill(0)
+          .map((_, index) => (
+            <Row
+              key={index}
+              fragmentClaimer={fragmentClaimer}
+              rowNumber={index}
+            />
+          ))}
+      </Grid>
+    </Paper>
   );
 };
